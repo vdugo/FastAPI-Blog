3a72ddd0119f37aabef138574925b6f8791b5528
Vincent <v3rdugo@yahoo.com>
2022-11-04 16:55:12 -0700

    include post router in main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+
 from database import models
 from database.database import engine
+from routers import post
 
 app = FastAPI()
+app.include_router(post.router)
 
 @app.get('/')
 def hello_world():
