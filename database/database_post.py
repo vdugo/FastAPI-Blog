ea392df1423142cae0beb1fad79c9e523f045fce
Vincent <v3rdugo@yahoo.com>
2022-11-04 16:44:00 -0700

    functionality for creating a DB post

---
+++ database/database_post.py
@@ -0,0 +1,18 @@
+import datetime
+
+from database.models import DbPost
+from routers.schemas import PostBase
+from sqlalchemy.orm.session import Session
+
+def create(db: Session, request: PostBase):
+    new_post = DbPost(
+        image_url = request.image_url,
+        title = request.title,
+        content = request.content,
+        creator = request.creator,
+        timestamp = datetime.datetime.now()
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
