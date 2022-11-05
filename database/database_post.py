673e8b6b6c063441a1ac5cdedc617277a76d65ed
Vincent <v3rdugo@yahoo.com>
2022-11-04 17:10:24 -0700

    add deleting a post functionality

--- database/database_post.py
+++ database/database_post.py
@@ -1,5 +1,7 @@
 import datetime
 
+from fastapi import HTTPException, status
+
 from database.models import DbPost
 from routers.schemas import PostBase
 from sqlalchemy.orm.session import Session
@@ -19,3 +21,11 @@ def create(db: Session, request: PostBase):
 
 def get_all_posts(db: Session):
     return db.query(DbPost).all()
+
+def delete_post(id: int, db: Session):
+    post_to_delete = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id {id} does not exist.')
+    db.delete(post_to_delete)
+    db.commit()
+    return f'Post with id {id} deleted.'
